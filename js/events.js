620dd57b27f517be1519f316491a3f10288a0689
noman-swe <noman.swe130@gmail.com>
2022-08-01 23:25:22 +0600

    25-3 Different ways to use addEventListener on a button

--- js/events.js
+++ js/events.js
@@ -9,3 +9,35 @@ redBtn.onclick = makeRed;
 function makeRed(){
     document.body.style.backgroundColor = 'red';
 }
+
+
+// 3
+//with Normal function 
+const greenBtn = document.getElementById('greenBtn');
+greenBtn.onclick = function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+}
+
+//with anonymous function
+const tomatoBtn = document.getElementById('tomatoBtn'); 
+tomatoBtn.onclick = function(){
+    document.body.style.backgroundColor = 'tomato';
+}
+
+// add EventListner
+const goldenRod = document.getElementById('goldenRodBtn');
+goldenRod.addEventListener('click', makeGoldenRod);
+
+function makeGoldenRod(){
+    document.body.style.backgroundColor = 'goldenrod';
+};
+
+// 
+const hotPinkBtn = document.getElementById('hotPinkBtn');
+hotPinkBtn.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'hotpink';
+});
+// direct
+document.getElementById('khakiBtn').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'khaki';
+});
