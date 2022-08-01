bbe31edcf046ec3f396453f6ec4348bce1a73a16
noman-swe <noman.swe130@gmail.com>
2022-08-01 22:54:37 +0600

    25-2 Add onclick handler directly or via javascript

---
+++ js/events.js
@@ -0,0 +1,11 @@
+function makeSkyblue(){
+    document.body.style.backgroundColor = 'skyblue';
+}
+
+// btn Red
+const redBtn = document.getElementById('redBtn');
+redBtn.onclick = makeRed;
+
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
