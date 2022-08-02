c58dfb4b24cb4fa9da3bc0a4092a2e4a86461e11
noman-swe <noman.swe130@gmail.com>
2022-08-02 10:50:04 +0600

    25-5 Create a comment box and display comment

---
+++ js/comment.js
@@ -0,0 +1,13 @@
+document.getElementById('post-btn').addEventListener('click', function(){
+    const commentField = document.getElementById('comment-field');
+    const userComment = commentField.value;
+
+    // create Element
+    const p = document.createElement('p'); 
+    p.innerText = userComment;
+
+    // 
+    const commentBox = document.getElementById('comment-container');
+    commentBox.appendChild(p);
+    commentField.value = '';
+})
